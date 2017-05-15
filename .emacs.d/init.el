0c972fb773db357b9bdeba4874dc36d73e1bef48
kyosuke <kyosuke.araki611@gmail.com>
2017-05-15 12:24:11 +0900

    ace-jump-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -61,6 +61,15 @@
   (load-library "migemo")
   (migemo-init))
 
+;; 画面内検索
+
+(require 'ace-jump-mode)
+(setq ace-jump-mode-gray-background nil)
+(setq ace-jump-word-mode-use-query-char nil)
+(setq ace-jump-mode-move-keys
+      (append "asdfghjkl;:]qwertyuiop@zxcvbnm,." nil))
+(global-set-key (kbd "C-o") 'ace-jump-word-mode)
+
 ;; フォント
 (set-face-attribute 'default nil :family "Ricty for Powerline")
 
