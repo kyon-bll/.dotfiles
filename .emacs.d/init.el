80637b13935371a3b5da97f70ceb1d066f0accb8
kyosuke <kyosuke.araki611@gmail.com>
2017-05-16 14:02:41 +0900

    modify ace-jump-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -66,7 +66,7 @@
 (setq ace-jump-mode-gray-background nil)
 (setq ace-jump-word-mode-use-query-char nil)
 (setq ace-jump-mode-move-keys
-      (append "asdfghjkl;:]qwertyuiop@zxcvbnm,." nil))
+      (append "asdfghjklqwertyuiopzxcvbnm,." nil))
 (global-set-key (kbd "C-o") 'ace-jump-word-mode)
 
 ;; 2画面ファイラー
