c4f70529f1527ccb5ccf6f918409542d67e263a6
kyosuke <kyosuke.araki611@gmail.com>
2017-05-18 21:14:26 +0900

    pyflake mini-buffer message

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -419,7 +419,26 @@
 ;(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
 ;;pyflakes：文法がただしいかを動的チェック
-(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+;(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory
+                         buffer-file-name))))
+      (list "/usr/local/bin/pyflakes"  (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+;; show message on mini-buffer
+(defun flymake-show-help ()
+  (when (get-char-property (point) 'flymake-overlay)
+    (let ((help (get-char-property (point) 'help-echo)))
+      (if help (message "%s" help)))))
+(add-hook 'post-command-hook 'flymake-show-help)
 
 ;;{M-n:次のエラーへ, M-p:前のエラーへ}
 (global-set-key "\M-n" 'flymake-goto-next-error)
