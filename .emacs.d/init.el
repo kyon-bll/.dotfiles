a708febc8efc3a7d3ac7a6d86ff97848fd76ea78
kyosuke <kyosuke.araki611@gmail.com>
2017-06-07 23:45:51 +0900

    pyflake error fix

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -427,21 +427,9 @@
 ;(setq flycheck-flake8--line-length 200)
 ;(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
-;;pyflakes：文法がただしいかを動的チェック
-;(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+;; pyflakes：文法がただしいかを動的チェック
+(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
 
-(add-hook 'find-file-hook 'flymake-find-file-hook)
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory
-                         buffer-file-name))))
-      (list "/usr/local/bin/pyflakes"  (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
 ;; show message on mini-buffer
 (defun flymake-show-help ()
   (when (get-char-property (point) 'flymake-overlay)
