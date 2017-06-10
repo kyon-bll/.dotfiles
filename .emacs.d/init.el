030f79556728f835f00377eadb9602a3df496bad
kyosuke <kyosuke.araki611@gmail.com>
2017-06-10 21:26:48 +0900

    utf-8マン

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,6 +14,9 @@
 (setq default-buffer-file-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (prefer-coding-system 'utf-8)
+(set-default 'buffer-file-coding-system 'utf-8)
+(setq coding-system-for-read 'utf-8)
+(setq coding-system-for-write 'utf-8)
 
 ;; mozc
 (require 'mozc)
