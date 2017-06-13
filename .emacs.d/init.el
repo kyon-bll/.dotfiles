01353bacde7206895f7123d3b12b5eb14ee7b079
kyosuke <kyosuke.araki611@gmail.com>
2017-06-14 00:49:43 +0900

    php

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -513,6 +513,24 @@
  )
 
 ;; ==================================
+;;                php
+;; ==================================
+
+;; php-mode
+(require 'php-mode)
+
+(require 'cl)
+(add-hook 'php-mode-hook
+          '(lambda ()
+             (auto-complete-mode t)
+             (require 'ac-php)
+             (setq ac-sources  '(ac-source-php ) )
+             (yas-global-mode 1)
+             (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
+             (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back   ) ;go back
+             ))
+
+;; ==================================
 ;;          twittering-mode
 ;; ==================================
 
