ddf42bff0a8773d5540fd7a78cb2b5f53bd8ce83
kyosuke <kyosuke.araki611@gmail.com>
2017-07-06 17:58:49 +0900

    Change zubora function output same as command line more perfectly

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -37,14 +37,14 @@ alias gc='git checkout'
 alias gl='git pull'
 function zubora -d "zubora 'commit message' [files to add;optional]"
     if test (count $argv) -gt 1
-        set_color -o; echo '-n \$ git ' ; set_color normal; set_color cyan; echo add $argv[2..-1]  
+        set_color -o; echo -n '\$ git ' ; set_color normal; set_color cyan; echo add $argv[2..-1]  
         set_color normal; git add $argv[2..-1]
     else
-        set_color -o; echo '-n \$ git ' ; set_color normal; set_color cyan; echo add -A
+        set_color -o; echo -n '\$ git ' ; set_color normal; set_color cyan; echo add -A
         set_color normal; git add -A
     end
-    set_color -o; echo '-n \$ git ' ; set_color normal; set_color cyan; echo '-n commit -m '; set_color yellow; echo "'"$argv[1]"'"
+    set_color -o; echo -n '\$ git ' ; set_color normal; set_color cyan; echo -n 'commit -m '; set_color yellow; echo "'"$argv[1]"'"
     set_color normal; git commit -m $argv[1]
-    set_color -o; echo '-n \$ git ' ; set_color normal; set_color cyan; echo push
+    set_color -o; echo -n '\$ git ' ; set_color normal; set_color cyan; echo push
     set_color normal; git push
 end
