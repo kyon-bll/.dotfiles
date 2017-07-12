b3235fde4f8f7c19905842e79251866a8d8ec4be
kyosuke <kyosuke.araki611@gmail.com>
2017-07-12 14:08:11 +0900

    FIx error (Delete ddskk package)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -59,7 +59,6 @@
 ;; ;; M-x skk-tutorialでNo file found as 〜とエラーが出たときにskk-tut-fileを設定
 ;; ;; make what-whereでSKK tutorialsで表示されるディレクトリ上のSKK.tutを指定
 ;; (setq skk-tut-file "/usr/share/skk/SKK.tut")
-;; ;; (require 'skk)
 ;; (require 'dired-x)
 ;; (global-set-key "\C-x\M-j" 'skk-mode)
 
