0988935a8427ed67928742a51ad5c6a8781f636b
kyosuke <kyosuke.araki611@gmail.com>
2018-03-27 18:26:08 +0900

    Remove cask files

--- .emacs.d/.cask/25.3/elpa/migemo-20160924.741/migemo-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; migemo-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("migemo.el") (23225 48992 997334 962000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; migemo-autoloads.el ends here
--- .emacs.d/.cask/25.3/elpa/prodigy-20180312.325/prodigy-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "prodigy" "20180312.325" "Manage external services from within Emacs" '((s "1.8.0") (dash "2.4.0") (f "0.14.0") (emacs "24")) :commit "67b74d1354616f4761acd947fb4f89c6c8bd758e" :url "http://github.com/rejeep/prodigy.el")
